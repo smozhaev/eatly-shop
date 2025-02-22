d23fcea9e5041d1843c48212ab39729137a9b743
smozhaev <kmozhaev100@gmail.com>
2025-02-22 14:24:57 +0300

    Add top dishes widget imports

--- src/pages/Home/ui/home-page/index.tsx
+++ src/pages/Home/ui/home-page/index.tsx
@@ -1,10 +1,9 @@
 import { PageContainer } from '@shared/ui';
-// import styles from './styles.module.scss';
+import { TopDishes } from '@widgets/top-dishes';
 import { HeroSection } from '../hero-section';
 import { Stats } from '../stats';
 import { AboutMobile } from '../about-mobile';
 import { TopRestaurant } from '../top-restaurants';
-import { TopDishes } from '@widgets/top-dishes';
 
 export const Home = () => {
     return (
